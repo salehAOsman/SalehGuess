808f8d80a88393728b54645589388fccbda8c4fc
Saleh Osman <saleh1.a.osman@gmail.com>
2018-01-21 17:57:12 +0100

    Add project files.

---
+++ SalehGessingNumber/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using SalehGessingNumber.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalehGessingNumber.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int guess)
+        {
+            
+            int randNum=guess;
+            if (Session["rand"] !=null && (string)TempData["newGame"]!="Won")//not finish
+            {
+                ModelNumber modNum = new ModelNumber(guess); //Id++
+
+                randNum = (int)Session["rand"];
+
+                if (guess > randNum)
+                {
+                    ViewBag.msg = "go down";
+                }
+                else if (guess < randNum)
+                {
+                    ViewBag.msg = "go upp";
+                }
+                else
+                {
+                    ViewBag.msg = "Won";
+                    TempData["newGame"] = ViewBag.msg;
+                }
+
+                modNum.Description = ViewBag.msg;                                                                    //add three properties with Id by constractor                                                                 
+                modNum.GuessNum = guess;
+
+                List<ModelNumber> listModel = new List<ModelNumber>();                                                   //new list to use it as stor for session["listGuessNum"] 
+
+                listModel = (List<ModelNumber>)Session["listGuess"];                                                     //store session in new list
+                listModel.Add(modNum);                                                                                      //we add new object ot list 
+                Session["listGuess"] = listModel;                                                                        //we add again list ot session["listGuessNum"] as stackable
+
+            }
+            else //first time to guess
+            {
+                Random rand = new Random();
+                ModelNumber modNum = new ModelNumber(guess); //Id++
+
+                Session["rand"] = rand.Next(1,101);
+                /*repiting section*/
+                randNum = (int)Session["rand"];
+
+                if (guess > randNum)
+                    {
+                        ViewBag.msg = "go down";
+                    }
+                else if (guess < randNum)
+                    {
+                      ViewBag.msg = "go upp";
+                    }
+                else
+                    {
+                        ViewBag.msg = "Won";
+                        TempData["newGame"] = ViewBag.msg;
+                    }
+
+                modNum.Description = ViewBag.msg;                                                                    //add three properties with Id by constractor                                                                 
+                modNum.GuessNum = guess;                                                                             //add object to list                                                                         
+
+                List<ModelNumber> listModel = new List<ModelNumber>();                                                   //new list
+                listModel.Add(modNum);                                                                                   // add object to list
+                Session["listGuess"] = listModel;             //???? how can I display this session as a result report   // save listModel in session["listGuess"] 
+                /*end repiting section*/
+            }
+            return View();
+        }
+    }
+}
---
+++ SalehGessingNumber/Models/ModelNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SalehGessingNumber.Models
+{
+    public class ModelNumber
+    {
+
+        [Display(Name ="Ordering")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="input number 1 --> 100")]
+        [Display(Name ="Guessing number")]
+        public int GuessNum { get; set; }
+        [Display(Name ="Description")]
+        public string Description { get; set; }
+
+        public ModelNumber()
+        {
+
+        }
+        
+        public ModelNumber(int guessNum)
+        {
+            Id++;
+            GuessNum = guessNum;
+        }
+    }
+}
